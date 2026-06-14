87c080030afe708ff3c30523773c6050051da31e
Vasyl <vasyllendyo91@gmail.com>
2026-06-14 15:10:23 +0300

    added saving theme

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let btn = document.getElementById('theme-toggle');
 
+if(localStorage.getItem('theme') === 'dark'){
+    document.body.classList.add('dark-theme');
+    btn.textContent = 'Сонце ☀️';
+}
+
 btn.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme');
     
     if (document.body.classList.contains('dark-theme')) {
         btn.textContent = 'Сонце ☀️';
+        localStorage.setItem('theme', 'dark');
     } else {
         btn.textContent = 'Місяць 🌙';
+        localStorage.setItem('theme', 'light');
     }
 });
